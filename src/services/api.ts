35eb81bcb3a182f67725c5795895805c63501a07
sleduardo20 <sl.eduardo20@gmail.com>
2020-09-01 14:19:58 -0300

    chore: api correct

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL:
-    'http://api.weatherapi.com/v1/forecast.json?key=49a17211b4614e59bb5194817202808&q=',
+    'https://api.weatherapi.com/v1/forecast.json?key=49a17211b4614e59bb5194817202808&q=',
 });
